ee03f23b3328f9d0e54114f6020c20e21e624a82
Jeno Laszlo <jeno.laszlo@betssongroup.com>
2022-10-28 17:29:23 +0200

    added tester script

---
+++ tester.ps1
@@ -0,0 +1,18 @@
+﻿$data = @()
+
+for ($i = 0; $i -lt 100; $i++) { $data += [Guid]::NewGuid().ToString() }
+
+$data | % {
+
+    for ($i = 0; $i -lt 10; $i++) {
+
+        Invoke-WebRequest -Method Post -Uri http://localhost:81/ -Body $_ -UseBasicParsing | % {
+            New-Object PsObject -Property @{
+                Cache = $_.Headers."X-Cached"
+                Content = [System.Text.Encoding]::UTF8.GetString($_.Content)
+            }
+        }
+
+    }
+
+}
