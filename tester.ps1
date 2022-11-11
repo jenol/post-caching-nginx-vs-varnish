3406b0b9fbcd9d0bd898568c385eb3bf4d9e1f56
Jeno Laszlo <jeno.laszlo@betssongroup.com>
2022-11-11 12:13:06 +0100

    pushing some prometheus and varnish image changes

--- tester.ps1
+++ tester.ps1
@@ -6,13 +6,23 @@ $data | % {
 
     for ($i = 0; $i -lt 10; $i++) {
 
-        Invoke-WebRequest -Method Post -Uri http://localhost:81/ -Body $_ -UseBasicParsing | % {
-            New-Object PsObject -Property @{
-                Cache = $_.Headers."X-Cached"
-                Content = [System.Text.Encoding]::UTF8.GetString($_.Content)
+        try {
+            Invoke-WebRequest -Method Post -Uri http://localhost:81 -Body $_ -UseBasicParsing | % {
+                New-Object PsObject -Property @{
+                    Cache = $_.Headers."X-Cached"
+                    Content = [System.Text.Encoding]::UTF8.GetString($_.Content)
+                }
+            }
+        } catch [System.Net.WebException] {
+            $e = $_.Exception
+            Write-host $e -ForegroundColor Red
+            $e.Response.Headers.AllKeys | % {
+                Write-host "$($_): $($e.Response.Headers[$_])" -ForegroundColor Red
             }
         }
-
     }
 
 }
+
+
+#http://sto-t-fecrs02.bde.local:9077/Content3/ContentService.svc/text
